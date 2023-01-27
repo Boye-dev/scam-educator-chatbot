de2c5377b9809092b4c216e796fc5cfc8b8d5f62
Boye-dev <oyelolaboye@gmail.com>
2023-01-27 13:53:44 +0100

    Testing routes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,8 +5,6 @@ module.exports = function (app) {
     "/api",
     createProxyMiddleware({
       target: "https://pnic.up.railway.app",
-      // target: 'https://westway-application.herokuapp.com',
-      // target: "https://westway.world",
       changeOrigin: true,
     })
   );
